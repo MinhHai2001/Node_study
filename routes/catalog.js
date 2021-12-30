b239b6ec53cc0b6562f6902643b6dfbe3c82f660
MinhHai2001 <hai.huynh22@student.passerellesnumeriques.org>
2021-12-30 14:31:12 +0700

    Part 6

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,117 +1,149 @@
-var express = require('express');
-var router = express.Router();
+const { body,validationResult } = require('express-validator');
+var Author = require('../models/author');
+var async = require('async');
+var Book = require('../models/book');
 
-// Require controller modules.
-var book_controller = require('../controllers/bookController');
-var author_controller = require('../controllers/authorController');
-var genre_controller = require('../controllers/genreController');
-var book_instance_controller = require('../controllers/bookinstanceController');
+// Display list of all Authors.
+exports.author_list = function(req, res, next) {
 
-/// BOOK ROUTES ///
+    Author.find()
+      .sort([['family_name', 'ascending']])
+      .exec(function (err, list_authors) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('author_list', { title: 'Author List', author_list: list_authors });
+      });
+  
+  };
 
-// GET catalog home page.
-router.get('/', book_controller.index);
+// Display detail page for a specific Author.
+exports.author_detail = function(req, res, next) {
 
-// GET request for creating a Book. NOTE This must come before routes that display Book (uses id).
-router.get('/book/create', book_controller.book_create_get);
+    async.parallel({
+        author: function(callback) {
+            Author.findById(req.params.id)
+              .exec(callback)
+        },
+        authors_books: function(callback) {
+          Book.find({ 'author': req.params.id },'title summary')
+          .exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); } // Error in API usage.
+        if (results.author==null) { // No results.
+            var err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render.
+        res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books } );
+    });
 
-// POST request for creating Book.
-router.post('/book/create', book_controller.book_create_post);
+};
 
-// GET request to delete Book.
-router.get('/book/:id/delete', book_controller.book_delete_get);
+// Display Author create form on GET.
+exports.author_create_get = function(req, res, next) {
+    res.render('author_form', { title: 'Create Author'});
+};
 
-// POST request to delete Book.
-router.post('/book/:id/delete', book_controller.book_delete_post);
+// Handle Author create on POST.
+exports.author_create_post = [
 
-// GET request to update Book.
-router.get('/book/:id/update', book_controller.book_update_get);
+    // Validate and sanitize fields.
+    body('first_name').trim().isLength({ min: 1 }).escape().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').trim().isLength({ min: 1 }).escape().withMessage('Family name must be specified.')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601().toDate(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601().toDate(),
 
-// POST request to update Book.
-router.post('/book/:id/update', book_controller.book_update_post);
+    // Process request after validation and sanitization.
+    (req, res, next) => {
 
-// GET request for one Book.
-router.get('/book/:id', book_controller.book_detail);
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
 
-// GET request for list of all Book items.
-router.get('/books', book_controller.book_list);
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('author_form', { title: 'Create Author', author: req.body, errors: errors.array() });
+            return;
+        }
+        else {
+    
 
-/// AUTHOR ROUTES ///
+            // Create an Author object with escaped and trimmed data.
+            var author = new Author(
+                {
+                    first_name: req.body.first_name,
+                    family_name: req.body.family_name,
+                    date_of_birth: req.body.date_of_birth,
+                    date_of_death: req.body.date_of_death
+                });
+            author.save(function (err) {
+                if (err) { return next(err); }
+                // Successful - redirect to new author record.
+                res.redirect(author.url);
+            });
+        }
+    }
+];
 
-// GET request for creating Author. NOTE This must come before route for id (i.e. display author).
-router.get('/author/create', author_controller.author_create_get);
+// Display Author delete form on GET.
+exports.author_delete_get = function(req, res, next) {
 
-// POST request for creating Author.
-router.post('/author/create', author_controller.author_create_post);
+    async.parallel({
+        author: function(callback) {
+            Author.findById(req.params.id).exec(callback)
+        },
+        authors_books: function(callback) {
+          Book.find({ 'author': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.author==null) { // No results.
+            res.redirect('/catalog/authors');
+        }
+        // Successful, so render.
+        res.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authors_books } );
+    });
 
-// GET request to delete Author.
-router.get('/author/:id/delete', author_controller.author_delete_get);
+};
 
-// POST request to delete Author.
-router.post('/author/:id/delete', author_controller.author_delete_post);
+// Handle Author delete on POST.
+exports.author_delete_post = function(req, res, next) {
 
-// GET request to update Author.
-router.get('/author/:id/update', author_controller.author_update_get);
+    async.parallel({
+        author: function(callback) {
+          Author.findById(req.body.authorid).exec(callback)
+        },
+        authors_books: function(callback) {
+          Book.find({ 'author': req.body.authorid }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        // Success
+        if (results.authors_books.length > 0) {
+            // Author has books. Render in same way as for GET route.
+            res.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authors_books } );
+            return;
+        }
+        else {
+            // Author has no books. Delete object and redirect to the list of authors.
+            Author.findByIdAndRemove(req.body.authorid, function deleteAuthor(err) {
+                if (err) { return next(err); }
+                // Success - go to author list
+                res.redirect('/catalog/authors')
+            })
+        }
+    });
+};
 
-// POST request to update Author.
-router.post('/author/:id/update', author_controller.author_update_post);
+// Display Author update form on GET.
+exports.author_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author update GET');
+};
 
-// GET request for one Author.
-router.get('/author/:id', author_controller.author_detail);
-
-// GET request for list of all Authors.
-router.get('/authors', author_controller.author_list);
-
-/// GENRE ROUTES ///
-
-// GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id).
-router.get('/genre/create', genre_controller.genre_create_get);
-
-//POST request for creating Genre.
-router.post('/genre/create', genre_controller.genre_create_post);
-
-// GET request to delete Genre.
-router.get('/genre/:id/delete', genre_controller.genre_delete_get);
-
-// POST request to delete Genre.
-router.post('/genre/:id/delete', genre_controller.genre_delete_post);
-
-// GET request to update Genre.
-router.get('/genre/:id/update', genre_controller.genre_update_get);
-
-// POST request to update Genre.
-router.post('/genre/:id/update', genre_controller.genre_update_post);
-
-// GET request for one Genre.
-router.get('/genre/:id', genre_controller.genre_detail);
-
-// GET request for list of all Genre.
-router.get('/genres', genre_controller.genre_list);
-
-/// BOOKINSTANCE ROUTES ///
-
-// GET request for creating a BookInstance. NOTE This must come before route that displays BookInstance (uses id).
-router.get('/bookinstance/create', book_instance_controller.bookinstance_create_get);
-
-// POST request for creating BookInstance.
-router.post('/bookinstance/create', book_instance_controller.bookinstance_create_post);
-
-// GET request to delete BookInstance.
-router.get('/bookinstance/:id/delete', book_instance_controller.bookinstance_delete_get);
-
-// POST request to delete BookInstance.
-router.post('/bookinstance/:id/delete', book_instance_controller.bookinstance_delete_post);
-
-// GET request to update BookInstance.
-router.get('/bookinstance/:id/update', book_instance_controller.bookinstance_update_get);
-
-// POST request to update BookInstance.
-router.post('/bookinstance/:id/update', book_instance_controller.bookinstance_update_post);
-
-// GET request for one BookInstance.
-router.get('/bookinstance/:id', book_instance_controller.bookinstance_detail);
-
-// GET request for list of all BookInstance.
-router.get('/bookinstances', book_instance_controller.bookinstance_list);
-
-module.exports = router;
+// Handle Author update on POST.
+exports.author_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author update POST');
+};
